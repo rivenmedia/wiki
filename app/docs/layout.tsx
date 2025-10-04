55d4f18ad8844954ceb8eda44ab02f26c011a603
davidemarcoli <davide@marcoli.ch>
2025-10-04 21:38:52 +0200

    first docs page

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -1,10 +1,19 @@
 import { DocsLayout } from 'fumadocs-ui/layouts/docs';
 import { baseOptions } from '@/lib/layout.shared';
+import { PageTree } from 'fumadocs-core/server';
 import { source } from '@/lib/source';
+
+const tree: PageTree.Root = {
+  name: 'docs',
+  children: [
+    { name: 'Services', type: 'separator' },
+    { name: 'Content', url: '/docs/services/content', type: 'page' },
+  ]
+}
 
 export default function Layout({ children }: LayoutProps<'/docs'>) {
   return (
-    <DocsLayout tree={source.pageTree} {...baseOptions()}>
+    <DocsLayout tree={tree} {...baseOptions()}>
       {children}
     </DocsLayout>
   );
--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -1,4 +1,5 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import Image from 'next/image';
 
 /**
  * Shared layout configurations
@@ -12,15 +13,16 @@ export function baseOptions(): BaseLayoutProps {
     nav: {
       title: (
         <>
-          <svg
+          {/* <svg
             width="24"
             height="24"
             xmlns="http://www.w3.org/2000/svg"
             aria-label="Logo"
           >
             <circle cx={12} cy={12} r={12} fill="currentColor" />
-          </svg>
-          My App
+          </svg> */}
+          <Image src="/logo.png" alt="Logo" width={24} height={24} />
+          Riven Wiki
         </>
       ),
     },
